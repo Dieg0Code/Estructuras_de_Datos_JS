604a8384112888649c85ee90c0c5f18beb6e970d
Dieg0code <diegoobando20@gmail.com>
2021-10-24 20:37:07 -0300

    agregar nodos a la lista

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -1,5 +1,5 @@
 // 1 --> 2 --> 3 --> 4 --> 5 --> null;
-
+/*
 let singlyLinkedList = {
     head: {
         value: 1,
@@ -15,6 +15,7 @@ let singlyLinkedList = {
         },
     },
 };
+*/
 
 class Node {
     constructor(value) {
@@ -32,6 +33,50 @@ class MySinglyLinkedList {
         this.tail = this.head;
 
         this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+
+        const newNode = new Node(value);
+        const firstPointer = this.getTheIndex(index - 1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = newNode;
+        newNode.next = holdingPointer;
+
+        this.length++;
+
+        return this;
+    }
+
+    getTheIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
     }
 }
 
